a020398a787f0537d401e2165d8920c9112e18a9
Jonathan Leung <me@jonl.org>
2014-10-04 20:20:17 -0600

    Stickies can be created and fetched

--- js/main.js
+++ js/main.js
@@ -1,25 +1,57 @@
 
 var BASE_URL = "http://localhost:3000"
-var Care = {}
+
+var Care = {};
+var Sticky = {};
+
 
 $(document).ready(function() {
 
 	Care.init = function() {
-		Care.loadStickies();
-		// Care.generateStickies();
+		Care.fetchAndDisplayStickies();
+		
+		var source = $("#sticky-template").html();
+		Sticky.fillTemplate = Handlebars.compile(source);
+
+		$("#form").submit(function(e) {
+			e.preventDefault();
+
+			var message = $(this).find(".input").val();
+			var params = {message: message};
+			Sticky.create(params).done(function(sticky) {
+				Sticky.data.push(sticky);
+				Care.appendSticky(sticky);
+			});
+		})
+	};
+
+	Care.appendStickies = function(stickies) {
+		$.each(stickies, function(i, sticky) {
+			Care.appendSticky(sticky);
+		});
+	};
+
+	Care.appendSticky = function(sticky) {
+		var newStickyHtml = Sticky.fillTemplate(sticky);
+		$("#stickies").append(newStickyHtml);
 	}
 
-	Care.loadStickies = function() {
-		Care.Stickies =
-
+	Care.fetchAndDisplayStickies = function() {
 		$.ajax({
 		  url: BASE_URL + "/wishes",
-		})
-		  .done(function( data ) {
-		  	Care.Cares = data;
+		}).done(function(data) {
+		  	Sticky.data = data;
+		  	Care.appendStickies(Sticky.data);
 		  });
-	}
+	};
 
+	Sticky.create = function(params) {
+		return $.ajax({
+			type: "POST",
+			url: BASE_URL + "/wishes",
+			data: params
+		})
+	}
 
 	Care.init();
 
